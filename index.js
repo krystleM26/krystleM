a4e8bb3601d8af6a09930a46f8d6aeb6bfae0ef0
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-05-24 11:44:53 -0700

    Adds to heroku

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+require('dotenv').config()
+const path = require('path')
+
 const express = require("express")
+
 const server = express()
+server.use(express.json())
+server.use(express.static(path.join(__dirname, "krystle-app/build")))
+
+server.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'krystle-app/build', 'index.html'))
+})
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'krystle-app/build', 'index.html' ))
+});
+
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => console.log('Server running on port ${PORT}'))
+
+
+
+
+server.listen(PORT, console.log('Server running on port ${PORT}'))
--- krystle-app/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html, app.js, app.css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
