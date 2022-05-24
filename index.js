ef78c1e1dada80c0de33344bbac7ea13369f4b96
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-05-24 12:34:43 -0700

    wip

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const express = require("express")
 
 const server = express()
 server.use(express.json())
-server.use(express.static(path.join(__dirname, "krystle-app/build")))
+server.use(express.static(path.join(__dirname, "client/build")))
 
 server.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'krystle-app/build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'krystle-app/build', 'index.html' ))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html' ))
 });
 
 const PORT = process.env.PORT || 4000;
