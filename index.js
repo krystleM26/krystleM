48a4162fae760eb750d56225e8e1c87b169e1450
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-05-24 12:43:23 -0700

    working in progress

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, "client/build")))
 
 server.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    // res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    res.sendFile('<div>hello world</div>');
 })
 
 const PORT = process.env.PORT || 4000;
