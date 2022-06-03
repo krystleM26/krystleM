6a3ae0fe96ab6672ec26c03917658fa8eb222bd1
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-06-03 12:48:58 -0700

    Fixing backend

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require('dotenv').config()
-const path = require('path')
-
 const express = require("express")
 
 const server = express()
 server.use(express.json())
-server.use(express.static(path.join(__dirname, "client/build")))
 
-server.get('*', (req,res) => {
-    // res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    res.sendFile('<div>hello world</div>');
-})
+
 
 const PORT = process.env.PORT || 4000;
 
+server.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    // res.sendFile('<div>hello world</div>');
+})
 
-
-
-
-server.listen(PORT, console.log('Server running on port ${PORT}'))
+server.listen(PORT, console.log(`Server running on port ${PORT}`))
