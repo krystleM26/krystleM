d287151a9ef1dea73fc18d8e8e242cb87c68076a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-06-03 12:53:59 -0700

    Test API is built

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const server = express()
 server.use(express.json())
 
 
+server.get('/api/contacts', (req,res) => {
+    res.json([
+        { id: 1, firstname: "Krystle", message: "Don't give up"}
+    ])
+    // res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    // res.sendFile('<div>hello world</div>');
+})
 
 const PORT = process.env.PORT || 4000;
 
-server.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    // res.sendFile('<div>hello world</div>');
-})
 
 server.listen(PORT, console.log(`Server running on port ${PORT}`))
